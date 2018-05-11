2db667d939e83de8c95696a84f29b9dca390f203
Tóth Gergely <tgely@gumisarok.hu>
2018-05-11 20:06:55 +0200

    Introduce Cookie Consent Managment

---
+++ includes/languages/english/modules/content/footer/cm_cookie_consent.php
@@ -0,0 +1,24 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2018 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_CONTENT_FOOTER_COOKIE_CONSENT_TITLE', 'Cookie Consent PopUp Block');
+  define('MODULE_CONTENT_FOOTER_COOKIE_CONSENT_DESCRIPTION', 'Adds Cookie Consent PopUp Block to the Footer Area of your site');
+
+  define('MODULE_CONTENT_FOOTER_COOKIE_CONSENT_MODAL_BLOCK_TITLE', 'Cookies');
+  define('MODULE_CONTENT_FOOTER_COOKIE_CONSENT_MODAL_BUTTON_TITLE', 'My Cookie Consent');
+  define('MODULE_CONTENT_FOOTER_COOKIE_CONSENT_MODAL_TITLE', 'Website Cookie Consent');
+  define('MODULE_CONTENT_FOOTER_COOKIE_CONSENT_TITLE_STRICT', 'Strict cookies');
+  define('MODULE_CONTENT_FOOTER_COOKIE_CONSENT_TITLE_OSCOMMERCE', 'Website session cookie');
+  define('MODULE_CONTENT_FOOTER_COOKIE_CONSENT_TITLE_CONSENT_COOKIE', 'Website cookie consent');
+  define('MODULE_CONTENT_FOOTER_COOKIE_CONSENT_TITLE_THIRD_PARTIES', 'Third Party Services');
+  define('MODULE_CONTENT_FOOTER_COOKIE_CONSENT_TITLE_FUNCTIONAL', 'Functional Cookies');
+  define('MODULE_CONTENT_FOOTER_COOKIE_CONSENT_MODAL_CONTENT', 'How to use this page');
